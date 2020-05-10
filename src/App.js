8bb2dfffcb6f999133056e3290ca4ff9b6f8953d
bilalkeskin <bilalkeskin_94@hotmail.com>
2020-05-10 06:05:43 +0300

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import CoinFlipper from './CoinFlipper';
+import React from "react";
+import "./App.css";
+import CoinFlipper from "./CoinFlipper";
 
 function App() {
   return (
